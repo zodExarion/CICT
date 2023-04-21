b1b3688a6c5571495223e8ec6ad10332b3997872
jeff <guevarajeff21@gmail.com>
2023-04-22 00:22:35 +0800

    new version

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit  } from '@angular/core';
 import * as $ from 'jquery';
 
+
 // declare const $: any;
 
 @Component({
--- src/app/pages/account-faculty/account-faculty.component.ts
+++ src/app/pages/account-faculty/account-faculty.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-faculty.component.css']
 })
 export class AccountFacultyComponent {
+  isPasswordVisible = false;
 
+  togglePasswordVisibility() {
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    this.isPasswordVisible = !this.isPasswordVisible;
+    passwordInput.type = this.isPasswordVisible ? 'text' : 'password';
+  }
 }
